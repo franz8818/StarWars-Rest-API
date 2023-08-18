c73f0fc4b70549b94837b0e106cbfb3edbf464c2
Franz Seidel <109550161+franz8818@users.noreply.github.com>
2023-08-18 21:36:10 +0000

    Fuck fork

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, People
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,14 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/people', methods=['GET'])
 def handle_hello():
+    all_people = People.query.all()
+    if len(all_people) < 1 :  
+        return jsonify({"msg": "Not found"}), 404
+    results = list(map(lambda item: item.serialize(), all_people))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+    return jsonify(results), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+#estas son las tablas 
+class People(db.Model):
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    image = db.Column(db.String(250), unique=True, nullable=False)
+    gender = db.Column(db.String(6), unique=True, nullable=False)
+    birth_year = db.Column(db.String(80), unique=False, nullable=False)
+    height = db.Column(db.String(80), unique=False, nullable=False)
+    mass = db.Column(db.String(80), unique=False, nullable=False)
+    hair_color = db.Column(db.String(80), unique=False, nullable=False)
+    skin_color = db.Column(db.String(80), unique=False, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<People %r>' % self.name
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "uid": self.uid,
+            "name": self.name,
+            "image": self.image,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
             # do not serialize the password, its a security breach
         }
